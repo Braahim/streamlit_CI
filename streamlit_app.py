76603c6cf372684948b6cc35c70e209235956894
Braahim <46976823+Braahim@users.noreply.github.com>
2022-10-09 14:10:09 +0200

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,23 @@
+import snowflake.connector 
+import streamlit as st
+
+
+def main():
+    st.header('Breakfast Menu')
+    st.text('Omega 3 & Blueberry Oatmeal')
+    st.text('Kale, Spinach & Rocket Smoothie')
+    st.text('Hard-Boiled Free-Range Egg')
+    with st.form("my_form"):
+    st.write("Inside the form")
+    slider_val = st.slider("Form slider")
+    checkbox_val = st.checkbox("Form checkbox")
+
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("slider", slider_val, "checkbox", checkbox_val)
+
+    st.write("Outside the form")
+
+if __name__ == "__main__":
+  main()
