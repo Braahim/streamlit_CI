00e66e678392725eeff1a1b5e1522fc7c57b6bc0
Smaoui Brahim <brahim.smaoui@inetum.com>
2022-10-09 19:10:31 +0200

    third com

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,26 +8,13 @@ def main():
     st.text('Kale, Spinach & Rocket Smoothie')
     st.text('Hard-Boiled Free-Range Egg')
     st.session_state.number = st.number_input(label= "Column number",min_value=1,step=1)
-
+    st.session_state.l = []
     if st.session_state.number:
         for i in range(st.session_state.number):
-            st.text_input('column name' + str(i))
+           st.session_state.l.append(st.text_input('column name' + str(i)))
     
-    with st.form("my_form"):
-        st.write("Inside the form")
-
-            
-        slider_val = st.slider("Form slider")
-        checkbox_val = st.checkbox("Form checkbox")
-
-
-
-        # Every form must have a submit button.
-        submitted = st.form_submit_button("Submit")
-    if submitted:
-        st.write("slider", slider_val, "checkbox", checkbox_val)
-
-    st.write("Outside the form")
+    if st.button("Créer le schéma"):
+        st.text(st.session_state.l)
 
 if __name__ == "__main__":
   main()
